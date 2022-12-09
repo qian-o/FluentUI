491771b48d0fbb0998df0617f3819bf9367e7fdd
熙 王 <1324771795@qq.com>
2022-12-10 00:55:33 +0800

    lottie支持

--- FluentUI.Demo/ViewModels/Page2ViewModel.cs
+++ FluentUI.Demo/ViewModels/Page2ViewModel.cs
@@ -1,13 +1,28 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using FluentUI.Demo.Models.Messages;
 using FluentUI.Demo.Views;
 using FluentUI.Design.Models;
+using System;
+using System.IO;
 
 namespace FluentUI.Demo.ViewModels
 {
     public partial class Page2ViewModel : BaseViewModel<Page2>
     {
+        [ObservableProperty]
+        private string lottie1;
+
+        [ObservableProperty]
+        private string lottie2;
+
+        public Page2ViewModel()
+        {
+            Lottie1 = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "Files", "120582-gdsc-modules.json");
+            Lottie2 = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "Files", "91881-design.json");
+        }
+
         [RelayCommand]
         private void GoToPage1()
         {
---
+++ FluentUI.Design/Controls/LottieView.cs
@@ -0,0 +1,95 @@
+﻿using SkiaSharp;
+using SkiaSharp.Skottie;
+using SkiaSharp.Views.Desktop;
+using SkiaSharp.Views.WPF;
+using System.IO;
+using System.Timers;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace FluentUI.Design.Controls
+{
+    public class LottieView : SKElement
+    {
+        #region Variable
+        private Timer _timer;
+        #endregion
+
+        #region DependencyProperty
+        public static readonly DependencyProperty FilePathProperty = DependencyProperty.Register(nameof(FilePath), typeof(string), typeof(LottieView), new PropertyMetadata(string.Empty, OnFilePathChanged));
+        public static readonly DependencyProperty AnimationProperty = DependencyProperty.Register(nameof(Animation), typeof(Animation), typeof(LottieView), new PropertyMetadata(null));
+        public static readonly DependencyProperty AutoSizeProperty = DependencyProperty.Register(nameof(AutoSize), typeof(bool), typeof(LottieView), new PropertyMetadata(false));
+
+        public string FilePath
+        {
+            get { return (string)GetValue(FilePathProperty); }
+            set { SetValue(FilePathProperty, value); }
+        }
+
+        public Animation Animation
+        {
+            get { return (Animation)GetValue(AnimationProperty); }
+            set { SetValue(AnimationProperty, value); }
+        }
+
+        public bool AutoSize
+        {
+            get { return (bool)GetValue(AutoSizeProperty); }
+            set { SetValue(AutoSizeProperty, value); }
+        }
+        #endregion
+
+        protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+        {
+            if (Animation != null)
+            {
+                SKCanvas canvas = e.Surface.Canvas;
+                canvas.Clear();
+
+                Animation.Render(canvas, new SKRect(0, 0, e.Info.Width, e.Info.Height));
+            }
+        }
+
+        private static void OnFilePathChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is LottieView lottieView && File.Exists(lottieView.FilePath))
+            {
+                lottieView.LoadAnimation();
+            }
+        }
+
+        private void LoadAnimation()
+        {
+            _timer?.Stop();
+            _timer?.Dispose();
+
+            if (Animation.TryCreate(new MemoryStream(File.ReadAllBytes(FilePath)), out Animation animation))
+            {
+                Animation = animation;
+
+                if (AutoSize)
+                {
+                    Width = Animation.Size.Width;
+                    Height = Animation.Size.Height;
+                }
+
+                double seconds = 0;
+                _timer = new Timer(1000d / Animation.Fps);
+                _timer.Elapsed += (a, b) =>
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        Animation.SeekFrameTime(seconds);
+                        InvalidateVisual();
+                        if (seconds > Animation.Duration.TotalSeconds)
+                        {
+                            seconds = 0;
+                        }
+                    });
+                    seconds += _timer.Interval / 1000d;
+                };
+                _timer.Start();
+            }
+        }
+    }
+}
